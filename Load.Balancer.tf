f81496678b53602fd1932a8b2a2818ed9e864817
kits26kat24 <k.tijani@hotmail.com>
2023-05-27 14:43:25 +0100

    LB WIP

--- Load.Balancer.tf
+++ Load.Balancer.tf
@@ -12,46 +12,30 @@ resource "aws_lb" "test3b_load_balancer" {
     Environment = "dev"
   }
 }
-resource "aws_lb_listener" "test3b_front_end_1" {
+resource "aws_lb_listener" "test3b_front_end" {
   load_balancer_arn = aws_lb.test3b_load_balancer.arn
   port              = "80"
   protocol          = "HTTP"
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.test3b-1.arn
+    target_group_arn = aws_lb_target_group.test3b.arn
   }
 }
-resource "aws_lb_listener" "test3b_front_end_2" {
-  load_balancer_arn = aws_lb.test3b_load_balancer.arn
-  port              = "80"
-  protocol          = "HTTP"
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.test3b-1.arn
-  }
-}
-resource "aws_lb_target_group" "test3b-1" {
+resource "aws_lb_target_group" "test3b" {
   name     = "Test3B-lb-tg-Amazon-Linux"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.testcase_main.id
 }
 
-resource "aws_lb_target_group" "test3b-1" {
-  name     = "Test3B-lb-tg-Ubuntu"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.testcase_main.id
-}
-
 resource "aws_lb_target_group_attachment" "test3b-1" {
-  target_group_arn = aws_lb_target_group.test3b-1.arn
+  target_group_arn = aws_lb_target_group.test3b.arn
   target_id        = aws_instance.Amazon-Linux-App.id
   port             = 80
 }
 
 resource "aws_lb_target_group_attachment" "test3b-2" {
-  target_group_arn = aws_lb_target_group.test3b-1.arn
+  target_group_arn = aws_lb_target_group.test3b.arn
   target_id        = aws_instance.Ubuntu-App.id
   port             = 80
 }
